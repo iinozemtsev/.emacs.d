86dcda949f94b995ae45f89519747ef0a10975a8
Ivan Inozemtsev <ivan.inozemtsev@xored.com>
2014-09-03 16:18:37 +0700

    syncup

--- init.el
+++ init.el
@@ -33,12 +33,12 @@
 	   "ЙЦУКЕН keyboard layout widely used in Russia (ISO 8859-5 encoding)
   in assuming that your default keyboard layout is dvorak"
 	   nil t t t t nil nil nil nil nil t)
-	  (quail-define-rules ("1" ?1) ("2" ?2) ("3" ?3) ("4" ?4) ("5" ?5) ("6" ?6) ("7" ?7) ("8" ?8) ("9" ?9) ("0" ?0) ("[" ?-) ("]" ?=) ("`" ?ё) 
-			      ("'" ?й) ("," ?ц) ("." ?у) ("p" ?к) ("y" ?е) ("f" ?н) ("g" ?г) ("c" ?ш) ("r" ?щ) ("l" ?з) ("/" ?х) ("=" ?ъ) 
+	  (quail-define-rules ("§" ?ё) ("1" ?1) ("2" ?2) ("3" ?3) ("4" ?4) ("5" ?5) ("6" ?6) ("7" ?7) ("8" ?8) ("9" ?9) ("0" ?0) ("[" ?_) ("]" ?=) ("`" ?ё) 
+			      ("'" ?Й) ("," ?ц) ("." ?у) ("p" ?к) ("y" ?е) ("f" ?н) ("g" ?г) ("c" ?ш) ("r" ?щ) ("l" ?з) ("/" ?х) ("=" ?ъ) 
 			      ("a" ?ф) ("o" ?ы) ("e" ?в) ("u" ?а) ("i" ?п) ("d" ?р) ("h" ?о) ("t" ?л) ("n" ?д) ("s" ?ж) ("-" ?э) ("\\" ?\\) 
 			      (";" ?я) ("q" ?ч) ("j" ?с) ("k" ?м) ("x" ?и) ("b" ?т) ("m" ?ь) ("w" ?б) ("v" ?ю) ("z" ?.) 
-			      ("!" ?!) ("@" ?\") ("#" ?') ("$" ?\;) ("%" ?%) ("^" ?:) ("&" ??) ("*" ?*) ("(" ?() (")" ?)) ("{" ?_) ("}" ?+) ("~" ?Ё) 
-			      ("\"" ?Й) ("<" ?Ц) (">" ?У) ("P" ?К) ("Y" ?Е) ("F" ?Н) ("G" ?Г) ("C" ?Ш) ("R" ?Щ) ("L" ?З) ("?" ?Х) ("+" ?Ъ) 
+			      ("±" ?Ё) ("!" ?!) ("@" ?\") ("#" ?') ("$" ?\;) ("%" ?%) ("^" ?:) ("&" ??) ("*" ?*) ("(" ?() (")" ?)) ("{" ?-) ("}" ?+) ("~" ?Ё) 
+			      ("\"" ?й) ("<" ?Ц) (">" ?У) ("P" ?К) ("Y" ?Е) ("F" ?Н) ("G" ?Г) ("C" ?Ш) ("R" ?Щ) ("L" ?З) ("?" ?Х) ("+" ?Ъ) 
 			      ("A" ?Ф) ("O" ?Ы) ("E" ?В) ("U" ?А) ("I" ?П) ("D" ?Р) ("H" ?О) ("T" ?Л) ("N" ?Д) ("S" ?Ж) ("_" ?Э) ("|" ?/) 
 			      (":" ?Я) ("Q" ?Ч) ("J" ?С) ("K" ?М) ("X" ?И) ("B" ?Т) ("M" ?Ь) ("W" ?Б) ("V" ?Ю) ("Z" ?,))
 	  (setq default-input-method "cyrillic-dvorak")))
@@ -64,6 +64,12 @@
 	    (interactive "sKEY: ")
 	    (set 'id (upcase id))
 	    (insert (format "[[http://jira4.xored.com/browse/%s][%s]]" id id)))
+
+	  (defun insert-bugzilla-link (id)
+	    "Insert org-mode link to bugzilla"
+	    (interactive "sKEY: ")
+	    (set 'id (upcase id))
+	    (insert (format "[[https://bugs.eclipse.org/bugs/show_bug.cgi?id=%s][#%s]]" id id)))
 
 	  (defun insert-ecl-link (id)
 	    "Insert org-mode link to jira"
@@ -118,7 +124,15 @@
 		(octave . t)
 		(sqlite . t)
 		(perl . t)
-		(ledger . t) )))
+		(ledger . t) ))
+	  (defun sacha/org-html-checkbox (checkbox)
+	    "Format CHECKBOX into HTML."
+	    (case checkbox (on "<span class=\"check\">&#x2713;</span>") ; checkbox (checked)
+		  (off "<span class=\"checkbox\">&#x2717;</span>")
+		  (trans "<code>[-]</code>")
+		  (t "")))
+	  (defadvice org-html-checkbox (around sacha activate)
+	    (setq ad-return-value (sacha/org-html-checkbox (ad-get-arg 0)))))
   :config (progn
 	    (require 'ob)
 	    (require 'ob-eval)
@@ -154,6 +168,7 @@ This function is called by `org-babel-execute-src-block'."
   :bind (("C-c s" . insert-support-link)
 	 ("C-c j" . insert-jira-link)
 	 ("C-c e" . insert-ecl-link)
+	 ("C-c b" . insert-bugzilla-link)
 	 ("s-t" . org-todo-list)
 	 ("s-s" . org-schedule)
 	 ("s-a" . org-agenda)
@@ -165,7 +180,17 @@ This function is called by `org-babel-execute-src-block'."
   :ensure t)
 
 (use-package graphviz-dot-mode
-  :init (setq graphviz-dot-view-command "dot -Tpng %s | open -f -a Preview.app")
+  :init (progn
+	  (setq graphviz-dot-view-command "dot -Tpng %s | open -f -a Preview.app")
+
+	  (defun my-dot-preview () "" (interactive)
+		 (let ((cb (current-buffer)))
+		   (save-buffer)
+		   (compile compile-command)
+		   (sleep-for 1)
+		   (graphviz-dot-preview)))
+	  (add-hook 'graphviz-dot-mode-hook (lambda () (add-hook 'after-save-hook 'my-dot-preview nil 'make-it-local))))
+  :bind (("s-p" . my-dot-preview))
   :ensure t)
 
 
@@ -192,8 +217,30 @@ This function is called by `org-babel-execute-src-block'."
   :ensure t)
 
 (use-package direx
-  :init (setq direx:closed-icon "▶ "
-	      direx:open-icon "▼ ")
+  :init (setq direx:closed-icon " ▶ "
+	      direx:open-icon " ▼ ")
+  :config (progn
+	    (defun mark-for-direx (buf) "Work in progress, dedicated windows for files opened in direx" (interactive "b")
+		   (if (not (boundp 'direx-windows))
+		       (setq direx-windows ()))
+		   (add-to-list 'direx-windows (cons (get-buffer buf) (selected-window))))
+
+	    (defun my-direx-open () "" (interactive)
+		   (if (not (boundp 'direx-windows))
+		       (setq direx-windows ()))
+
+		   (setq item (direx:item-at-point!))
+		   (if (direx:item-leaf-p item)
+		       (let ((filename (direx:file-full-name (direx:item-tree item)))
+			     (wnd (cdr (assoc (current-buffer) direx-windows))))
+			 (let ((filebuf (find-file-noselect filename)))
+			   (if wnd
+			       (set-window-buffer wnd filebuf)
+			     (find-file-other-window filename))))))
+	    
+	    (bind-key (kbd "RET") 'my-direx-open direx:direx-mode-map)
+	    (bind-key (kbd "C-o") 'my-direx-open direx:direx-mode-map)
+	    )
   :bind ("C-x j" . direx:jump-to-directory)
   :ensure t)
 
@@ -302,3 +349,45 @@ This function is called by `org-babel-execute-src-block'."
 
 (defun kill-back () "" (interactive) (kill-sexp -1))
 (global-set-key [C-M-backspace] 'kill-back)
+
+(use-package haskell-mode
+  :init (progn
+	  (define-key haskell-mode-map (kbd "C-x C-d") nil)
+	  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+	  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+	  (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
+	  (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+	  (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+	  (define-key haskell-mode-map (kbd "C-c M-.") nil)
+	  (define-key haskell-mode-map (kbd "C-c C-d") nil)
+	  (setq haskell-process-path-ghci "/Users/ivaninozemtsev/ghc/bin/ghci"))
+  :ensure t)
+(use-package php-mode :ensure t)
+
+
+(defun my-replace () "" (interactive)
+       (mapcar (lambda (replacement)
+		 (try-replace
+		  (cdr (assoc :from replacement))
+		  (cdr (assoc :to replacement))
+		  (cdr (assoc :offset replacement))))
+	       (list
+		(make-replacement-alist "->" "&#8594;" 0)
+		(make-replacement-alist "..." "&#8230;" 0)		
+		(make-replacement-alist "--" "&ndash;" 0)
+		(make-replacement-alist "<u" "<span class=\"uiElement\"></span>" 7))))
+ 
+(defun make-replacement-alist (from to offset)
+  (cons (cons :from from) (cons (cons :to to) (cons (cons :offset offset) nil))))
+
+(defun try-replace (from to off)
+  (let* ((len (length from))
+	      (pos (point))
+	      (txt (buffer-substring-no-properties (- pos len) pos)))
+	 (when (string= txt from)
+	   (delete-char (- len))
+	   (insert to)
+	   (backward-char off))))
+
+(global-set-key (kbd "s-/") 'my-replace)
+
