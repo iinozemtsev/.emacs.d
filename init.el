00d50d06f2907640f420bec3d09ecf16ff7668bc
Ivan Inozemtsev <iinozemtsev@google.com>
2024-07-05 14:24:41 +0200

    Switch to straight.el, add vertico

--- init.el
+++ init.el
@@ -2,7 +2,25 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+'(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 (require 'use-package)
 
@@ -11,6 +29,37 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (blink-cursor-mode 0)
+
+(use-package general :straight t)
+
+(use-package vertico
+  ;; Special recipe to load extensions conveniently
+  :straight (vertico :files (:defaults "extensions/*")
+                     :includes (vertico-indexed
+                                vertico-flat
+                                vertico-grid
+                                vertico-mouse
+                                vertico-quick
+                                vertico-buffer
+                                vertico-repeat
+                                vertico-reverse
+                                vertico-directory
+                                vertico-multiform
+                                vertico-unobtrusive
+                                ))
+  :general
+  (:keymaps 'vertico-map
+   "<tab>" #'vertico-insert    ; Choose selected candidate
+   "<escape>" #'minibuffer-keyboard-quit ; Close minibuffer
+   ;; NOTE 2022-02-05: Cycle through candidate groups
+   "C-M-n" #'vertico-next-group
+   "C-M-p" #'vertico-previous-group)
+  :custom
+  (vertico-count 13)                    ; Number of candidates to display
+  (vertico-resize t)
+  (vertico-cycle nil) ; Go from last to first candidate and first to last (cycle)?
+  :config
+  (vertico-mode))
 
 (if (display-graphic-p)
     (scroll-bar-mode -1))
@@ -21,11 +70,11 @@
 (use-package window-numbering
   :init 
   (window-numbering-mode 1)
-  :ensure t)
+  :straight t)
 
 (use-package magit 
   :bind (("M-g s" . magit-status))
-  :ensure t)
+  :straight t)
 
 (set-frame-font "JetBrains Mono 12" nil t)
 
@@ -44,10 +93,13 @@
                               "|||>" "<|||" "<|>" "..." ".." ".=" "..<" ".?" "::" ":::" ":=" "::="
                               ":?" ":?>" "//" "///" "/*" "*/" "/=" "//=" "/==" "@_" "__" "???"
                               "<:<" ";;;"))
-  (global-ligature-mode t) :ensure t)
+  (global-ligature-mode t) :straight t)
 
 (if (eq system-type 'darwin)
     (progn
       (setq mac-command-modifier 'meta)
       (setq mac-right-command-modifier 'super)
       (set-frame-font "JetBrains Mono 20" nil t)))
+
+(use-package dart-mode :straight t)
+(use-package markdown-mode :straight t)
