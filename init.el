791f8a26095422be7b709664bdb48fd0f5c5b6c3
Ivan Inozemtsev <ivan.inozemtsev@gmail.com>
2024-09-09 16:48:43 +0200

    Sync latest changes from linux laptop

--- init.el
+++ init.el
@@ -28,6 +28,8 @@
 (setq ring-bell-function 'ignore)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
+(scroll-bar-mode -1)
+
 (blink-cursor-mode 0)
 
 (use-package general :straight t)
@@ -121,3 +123,20 @@
 
 (use-package dart-mode :straight t)
 (use-package markdown-mode :straight t)
+(use-package vscdark-theme :straight t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes nil)
+ '(custom-safe-themes
+   '("449a6a03953bf946290f1a0eac09a7de2cf2556aa890f28ad2510a494eb23dcb" "993aac313027a1d6e70d45b98e121492c1b00a0daa5a8629788ed7d523fe62c1" default))
+ '(package-selected-packages
+   '(vscdark-theme vscode-dark-plus-theme dart-mode ligature window-numbering magit lua-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
