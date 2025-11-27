f5096aa3451defc34a54b0c8f8b2b9cb33238ad2
Ivan Inozemtsev <ivan.inozemtsev@gmail.com>
2025-11-27 10:34:40 +0100

    push from linux laptop

--- custom.el
+++ custom.el
@@ -3,8 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(backup-directory-alist '(("." . "~/.emacs-saves")))
+ '(custom-enabled-themes nil)
+ '(custom-safe-themes
+   '("449a6a03953bf946290f1a0eac09a7de2cf2556aa890f28ad2510a494eb23dcb"
+     "993aac313027a1d6e70d45b98e121492c1b00a0daa5a8629788ed7d523fe62c1"
+     default))
  '(package-selected-packages
-   '(markdown-mode dart-mode ligature window-numbering magit lua-mode)))
+   '(vscdark-theme vscode-dark-plus-theme dart-mode ligature
+		   window-numbering magit lua-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
