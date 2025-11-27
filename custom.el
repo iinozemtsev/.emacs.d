08a98b00c2d6937e77773dbfda6643c23b790aaf
Ivan Inozemtsev <ivan.inozemtsev@gmail.com>
2025-11-27 23:37:33 +0100

    tramp allow unsafe

--- custom.el
+++ custom.el
@@ -6,12 +6,10 @@
  '(backup-directory-alist '(("." . "~/.emacs-saves")))
  '(custom-enabled-themes nil)
  '(custom-safe-themes
-   '("449a6a03953bf946290f1a0eac09a7de2cf2556aa890f28ad2510a494eb23dcb"
-     "993aac313027a1d6e70d45b98e121492c1b00a0daa5a8629788ed7d523fe62c1"
-     default))
+   '("449a6a03953bf946290f1a0eac09a7de2cf2556aa890f28ad2510a494eb23dcb" "993aac313027a1d6e70d45b98e121492c1b00a0daa5a8629788ed7d523fe62c1" default))
  '(package-selected-packages
-   '(vscdark-theme vscode-dark-plus-theme dart-mode ligature
-		   window-numbering magit lua-mode)))
+   '(vscdark-theme vscode-dark-plus-theme dart-mode ligature window-numbering magit lua-mode))
+ '(tramp-allow-unsafe-temporary-files t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
